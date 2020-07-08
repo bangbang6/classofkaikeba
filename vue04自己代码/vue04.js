dfe998ef2f501be3c8b89cc5b7ff1a818cc9f66d
bangbang6 <577174667@qq.com>
2020-07-08 10:47:30 +0800

    dep的理解

--- vue04自己代码/vue04.js
+++ vue04自己代码/vue04.js
@@ -1,5 +1,9 @@
 //commonjs 后台模块
 //暗号“BBQ”
 //data对象每个key都是dep 然后每个对象都是observer实例 data:{a:1,obj:{b:1}}假如val还是对象 则obj继续observer实例 第一个a:1不会创建Ibserver实例因为
-//会直接返回 记住一个对象一个observe data和obj 然后dep是data下面每个key都是dep这里有两个 通知更新 加上小管家是有observer实例也就是obj的时候有一个负责管理该Obj的通知
+//会直接返回 记住一个对象一个observe
 //watcher一个组件一个 dep和watcher多对多
+//data:{obj:{a:1,b:2}} 一个对象一个ob 一个组件一个watcher 一个key一个dep
+//obj有个dep他管理这个对象 比如 this.obj.c= 1
+//a有个dep管理a属性 比如this.a = 1
+//b同a
